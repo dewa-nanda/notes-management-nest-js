1eb3ed5c6c0d7986a870824d3dd8f7196f5348cf
Dewa Putra Hernanda <dewananda089@gmail.com>
2026-01-07 08:56:16 +0700

    fix: except eslint

--- src/modules/notes/entity/notes.entity.spec.ts
+++ src/modules/notes/entity/notes.entity.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesEntity } from './notes.entity';
 import { PrismaService } from '@src/database/prisma.service';
