ad077484882a343fc86c27bbcac994ed08684f5c
Dewa Putra Hernanda <dewananda089@gmail.com>
2026-01-10 10:15:27 +0700

    fix: change auth route into public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,17 +3,19 @@ import { RegisterAuthDto } from './dto/register-auth.dto';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+import { Public } from '@src/common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @Public()
   @Post('register')
   async register(@Body() dto: RegisterAuthDto) {
     return this.authService.register(dto);
   }
 
+  @Public()
   @Post('login')
   async login(@Body() dto: LoginAuthDto) {
     return this.authService.login(dto);
